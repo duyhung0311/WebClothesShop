a82eedafbc55f62a06f1725ed3550e38e4f4e1e0
duyhung0311 <56124429+duyhung0311@users.noreply.github.com>
2020-11-16 16:49:45 +0700

    product page

--- Front-end/webclothesshop/src/config/image.js
+++ Front-end/webclothesshop/src/config/image.js
@@ -5,6 +5,7 @@ export const Images={
     SLIDER_2:IMAGE_URL +"/static/img/slider_2.jpg",
     SLIDER_3:IMAGE_URL +"/static/img/slider_3.jpg",
     SLIDER_4:IMAGE_URL +"/static/img/slider_4.jpg",
-
+    PRODUCT_1:IMAGE_URL+"/static/img/product.jpg",
     
+    BANNER:IMAGE_URL+"/static/img/banner.jpg",
 };
--- Front-end/webclothesshop/src/feature/product/view/index.js
+++ Front-end/webclothesshop/src/feature/product/view/index.js
@@ -1,24 +1,137 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './style.css'
-import {Card} from 'antd'
+import {Card,Row,Col,Button} from 'antd'
+import { Images } from '../../../config/image'
+import Navbar from '../../../components/navbar'
 function Product() {
+    
     return (
+        <>
+                <Navbar/>
+
         <div className="container">
+            
             <div className="menu-container" >
-                <Card  title="Type Product" style={{ width: "100%"  }}>
-                    <p style={{fontFamily:"Bree Serif , serif"}}>Tee</p>
-                    <p>T-shirt</p>
-                    <p>Trouser</p>
+                <Card  bordered={false} style={{color:'blue'}}>
+                    <div className="title">Categories</div>
+                    <div className="item">
+                        <p>Tee</p>
+                        <p>T-shirt</p>
+                        <p>Blazer</p>
+                        <p>Pants</p>
+                        <p>Wide Pants</p>
+                    </div>
+                    
                 </Card>
-                <Card title="Price" style={{ width: "100%"  }}>
-                    <p>Under 1 million</p>
-                    <p>Between 1 million and 5 million</p>
-                    <p>Over 5 million</p>
-                </Card>
+                
             </div>
 
-            <div className="product-container"></div>
+            <div className="product-container"> 
+                
+                 <div className="banner-product" >
+                    <img  src={Images.BANNER} />
+                </div>
+                <Row>                   
+                    <Col span={8}>
+                        <div className="product-inline" >
+                            <div className="image-product"  >
+                                <img src={Images.PRODUCT_1}  />
+                            </div> 
+                            
+                            <div className="content-product">
+                               <div className="detailed-content">Basic Tee 
+                                </div>
+                               <div className="price-product">270.000 VND</div>
+                               <div className="outline-button" > 
+                                    <Button type="primary" className="buying-rightnow" >Mua Ngay</Button>                                 
+                               </div>
+                            </div>
+                        </div>                       
+                    </Col>
+                    <Col span={8}>
+                        <div className="product-inline" >
+                            <div className="image-product">
+                                <img src={Images.PRODUCT_1} />
+                            </div> 
+                            <div className="content-product">
+                               <div className="detailed-content">Basic Tee 
+                                </div>
+                               <div className="price-product">270.000 VND</div>
+                               <div className="outline-button" > 
+                                    <Button type="primary" className="buying-rightnow" >Mua Ngay</Button>                                 
+                               </div>
+                            </div>
+                        </div>
+                       
+                    </Col>
+                    <Col span={8}>
+                        <div className="product-inline" >
+                            <div className="image-product">  <img src={Images.PRODUCT_1} /></div> 
+                            <div className="content-product">
+                               <div className="detailed-content">Basic Tee 
+                                </div>
+                               <div className="price-product">270.000 VND</div>
+                               <div className="outline-button" > 
+                                    <Button type="primary" className="buying-rightnow" >Mua Ngay</Button>                                 
+                               </div>
+                            </div>
+                        </div>
+                       
+                    </Col>
+                </Row>
+
+                <Row>                   
+                    <Col span={8}>
+                        <div className="product-inline" >
+                            <div className="image-product">
+                                <img src={Images.PRODUCT_1} />
+                            </div> 
+                            <div className="content-product">
+                               <div className="detailed-content">Basic Tee 
+                                </div>
+                               <div className="price-product">270.000 VND</div>
+                               <div className="outline-button" > 
+                                    <Button type="primary" className="buying-rightnow" >Mua Ngay</Button>                                 
+                               </div>
+                            </div>
+                        </div>                       
+                    </Col>
+                    <Col span={8}>
+                        <div className="product-inline" >
+                            <div className="image-product">
+                                <img src={Images.PRODUCT_1} />
+                            </div> 
+                            <div className="content-product">
+                               <div className="detailed-content">Basic Tee 
+                                </div>
+                               <div className="price-product">270.000 VND</div>
+                               <div className="outline-button" > 
+                                    <Button type="primary" className="buying-rightnow" >Mua Ngay</Button>                                 
+                               </div>
+                            </div>
+                        </div>
+                       
+                    </Col>
+                    <Col span={8}>
+                        <div className="product-inline" >
+                            <div className="image-product">  <img src={Images.PRODUCT_1} /></div> 
+                            <div className="content-product">
+                               <div className="detailed-content">Basic Tee 
+                                </div>
+                               <div className="price-product">270.000 VND</div>
+                               <div className="outline-button" > 
+                                    <Button type="primary" className="buying-rightnow" >Mua Ngay</Button>                                 
+                               </div>
+                            </div>
+                        </div>
+                       
+                    </Col>
+                </Row>
+                            
+               
+            </div>
         </div>
+        </>
     )
 }
 

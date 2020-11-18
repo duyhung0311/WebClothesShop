fde8bd1893dcb45dd75c8b0f83c8d8d878ea58e2
duyhung0311 <56124429+duyhung0311@users.noreply.github.com>
2020-11-18 10:17:51 +0700

    Router login,home,product,singleproduct page

--- Front-end/webclothesshop/src/App.js
+++ Front-end/webclothesshop/src/App.js
@@ -5,14 +5,42 @@ import Homepage from './feature/homepage/index';
 import Product from './feature/product/view/index'
 import SingleProduct from './feature/singleProduct/view';
  './feature/singleProduct/view/index'
+ import { BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      {/* <Homepage/> */}
-      {/* <Login/> */}
-      {/* <Product/> */}
-      <SingleProduct/>
-    </div>
+    <Router>
+      <div className="App">
+              {/* <Homepage/> */}
+              {/* <Login/> */}
+              {/* <Product/> */}
+              {/* <SingleProduct/> */}
+        
+            <Link to="/home" ></Link>
+            <Link to="/login" ></Link>
+            <Link to="/singleproduct"></Link>
+            <Link to="/product"></Link>
+
+            <Switch>
+        <Route path="/home" >
+          <Homepage/>
+        </Route>
+        <Route path="/login" >
+          <Login/>
+        </Route>
+        <Route path="/product">
+          <Product/>
+        </Route>
+        <Route path="/singleproduct">
+          <SingleProduct/>
+        </Route>
+      </Switch>
+      </div>
+
+     
+
+    </Router>
+    
+    
   );
 }
 
